f39c5e0c072f4d271f7addc5850c634ed8479693
Polyuso <Polyuso.info@gmail.com>
2017-07-06 22:35:50 +0200

    Update Main.js

--- Game1/Main.js
+++ Game1/Main.js
@@ -1,20 +1,49 @@
-
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create, render: render });
-
-var floor;
-
-function create() {
-    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    this.scale.pageAlignHorizontally = true;
-    this.scale.pageAlignVertically = true;
-    this.scale.setScreenSize( true );
-
-    floor = new Phaser.Rectangle(0, 550, 800, 49);
-
+function GamePlayer(){
+	this.Position = createVector(0, 0, 0);
+	
+	this.update = function(){
+		this.Position.add(0, 1, 0);
+		if (!keyIsPressed)
+			return -1;
+		if (key == "w")
+			this.Position.add(0, 0, -1);
+		if (key == "s")
+			this.Position.add(0, 0, 1);
+		if (key == "a")
+			this.Position.add(-1, 0, 0);
+		if (key == "d")
+			this.Position.add(1, 0, 0);
+		
+		
+	}
+	
+	this.draw = function(){
+		fill(0, 0, 0);
+		translate(this.Position.x, this.Position.y, this.Position.z);
+		box(10);
+		noFill();
+	}
+	
 }
 
-function render () {
+var Player;
+var Map;
 
-    game.debug.geom(floor,'#0fffff');
+function setup() {
+	createCanvas(800, 600, WEBGL);
+	background(20, 158, 204);
+	frameRate(30);
+	Player = new GamePlayer();
+}
 
+function draw() {
+	clear();
+	background(20, 158, 204);
+	translate(-width/2, -height/2);
+	
+	
+	Player.update();
+	
+	
+	Player.draw();
 }
